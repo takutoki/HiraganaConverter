869d79fa648c8706d2b44d06a0392b674ce2af2e
takutokiks <34600200+takutoki@users.noreply.github.com>
2019-09-09 17:35:02 +0900

    Modelを作成

--- HiraganaConverter/AppDelegate.swift
+++ HiraganaConverter/AppDelegate.swift
@@ -28,6 +28,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
     }
     
+    private func setUpGooAPI() {
+        let appID = "42a130a4a62eb8aa2552e5fcc89fd9ee00f36c0a9c6122b119a5797e468e9ac7"
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ HiraganaConverter/Models/HiraganaConverterModel.swift
@@ -0,0 +1,28 @@
+//
+//  HiraganaConverterModel.swift
+//  HiraganaConverter
+//
+//  Created by takumitokiwa on 2019/09/09.
+//  Copyright © 2019 takutoki. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+protocol HiraganaConverterModelProtocol: AnyObject {
+    
+    var hiraganaText: BehaviorRelay<String> { get set }
+    var hiraganaStream: Observable<String> { get }
+}
+
+class HiraganaConverterModel: HiraganaConverterModelProtocol {
+    
+    var hiraganaText: BehaviorRelay<String> = BehaviorRelay(value: "")
+    var hiraganaStream: Observable<String> { return hiraganaText.asObservable()}
+    
+    init() {}
+    
+    func request(sentence: String) {
+        
+    }
+}
